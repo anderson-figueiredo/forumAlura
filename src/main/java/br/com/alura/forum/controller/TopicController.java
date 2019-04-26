292782e5984074d998aa822024604d6affd63088
Anderson Figueiredo <figueiredo.anderson@rocketmail.com>
2019-04-26 01:19:16 -0300

    [CONTROLLER] Changing Topic to TopicBreafOutputDto

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -3,10 +3,12 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto;
 import br.com.alura.forum.model.Category;
 import br.com.alura.forum.model.Course;
 import br.com.alura.forum.model.User;
@@ -16,12 +18,13 @@ import br.com.alura.forum.model.topic.domain.Topic;
 public class TopicController {
 
 	@ResponseBody
-	@GetMapping
-	public List<Topic> listTopics() {
+	@GetMapping(value="/api/topics", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<TopicBriefOutputDto> listTopics() {
 		Category subCategory = new Category("Scrum", new Category("Negócio"));
 		Course course = new Course("Gerenciamento ágil com Scrum", subCategory);
 		Topic topic = new Topic("Métricas", "Como definir boas métricas para o time", new User("Bina", "bina@contato.com", "temcerteza"), course);
 		
-		return Arrays.asList(topic, topic, topic);
+		List<Topic> topics = Arrays.asList(topic, topic, topic);
+		return TopicBriefOutputDto.listFromTopics(topics);
 	}
 }
